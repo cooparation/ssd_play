a93e6b07f3e8aaccdbd7456c06df77c22d1cb207
SanjunLiu <sanjun_liu@sina.com>
2017-11-16 17:34:24 +0800

    clean the codes

--- 2_createTrainVal.py
+++ 2_createTrainVal.py
@@ -5,19 +5,20 @@ import global_dir
 from random import shuffle
 from collections import OrderedDict
 
-imgpath = ['testLabelImgs/JPEGImages']
+data_dir = '/apps/liusj/testDetection'
+paths = [data_dir + '/JPEGImages']
 file_type_list =['GIF', 'gif', 'jpeg',  'bmp', 'png', 'JPG',  'jpg', 'JPEG']
 #file_type_list = ['jpg']
 
 filenames = OrderedDict()
 write_lines = []
 types = set()
-for path in imgpath:
+for path in paths:
     for root, _, files in os.walk(path):
         for fname in files:
             types.add(fname.split('.')[-1])
             if fname.split('.')[-1] in file_type_list:
-                file_path = os.path.join(root,fname)
+                file_path = os.path.join(root, fname)
                 print 'file_path', file_path
                 label_name = file_path.split(path+'/')[-1]
                 label_name = label_name.split('/')[0]
@@ -28,15 +29,14 @@ for path in imgpath:
                     label = len(filenames)
                     filenames[label_name] =[label,1]
                 # print(cv2.imread(file_path).shape)
-                # write_lines.append(file_path+' '+str(label)+'\n')
                 write_lines.append((file_path.split('/')[-1]).split('.')[0] + '\n')
 shuffle(write_lines)
-print(types)
+
 L  = int(len(write_lines)*0.1)
-f = open('./testLabelImgs/ImageSets/Main/test.txt','w')
+f = open(data_dir + '/ImageSets/Main/test.txt','w')
 f.writelines(write_lines[:L])
 f.close()
 
-f = open('./testLabelImgs/ImageSets/Main/trainval.txt','w')
+f = open(data_dir + '/ImageSets/Main/trainval.txt','w')
 f.writelines(write_lines[L:])
 f.close()
